d371a05cb3d52f14be3efef4b1424260445ce560
Imelda Hope <imeldalarusso@gmail.com>
2024-07-15 08:41:12 +0300

    function definitions

---
+++ index.js
@@ -0,0 +1,49 @@
+//Implementation of collection-processing methods
+
+function myEach(collection, cb){
+
+}
+
+function myMap(collection, cb){
+
+}
+
+function myReduce(collection, cb, accum){
+
+}
+
+function myFind(collection, predicate){
+
+}
+
+function myFilter(collection, predicate){
+
+}
+
+function mySize(collection){
+
+}
+
+function myFirst(arr, n){
+
+}
+
+function myLast(arr, n){
+
+}
+
+function mySortBy(arr, cb){
+
+}
+
+function myFlatten(arr, shallow, newArr = []){
+
+}
+
+function myKeys(obj){
+
+}
+
+function myValues(obj){
+    
+}
