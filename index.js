2a8380b5baaac89cb658ab55adc3e5eac199ed01
Imelda Hope <imeldalarusso@gmail.com>
2024-07-15 10:38:35 +0300

    creates a function library

--- index.js
+++ index.js
@@ -81,11 +81,31 @@ function myLast(arr, n){
 }
 
 function mySortBy(arr, cb){
-
+    const newArr = [...arr];
+    return newArr.sort(function(a, b) {
+       if (cb(a) > cb(b)) {
+            // If cb(a) is greater than cb(b), a should come after b
+            return 1;
+        } else if (cb(b) > cb(a)) {
+            // If cb(b) is greater than cb(a), b should come after a
+            return -1;
+        } else {
+            return 0;
+        }
+    });
 }
 
 function myFlatten(arr, shallow, newArr = []){
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (Array.isArray(arr[i]) && !shallow) {
+            myFlatten(arr[i], false, newArr); // Recursively flatten nested arrays
+        } else {
+            newArr.push(arr[i]); // Add non-array elements to newArr
+        }
+    }
 
+    return newArr;   
 }
 
 function myKeys(obj){
